94649ca5a010084b038d429739a4addfe77bec94
sanedosa <sanedosa99@gmail.com>
2024-03-03 19:49:15 +0300

    consensus and rec sites solutions

---
+++ Consensus and Profile.py
@@ -0,0 +1,42 @@
+
+
+# f =open("test.txt", "r")
+# dna=[]
+# for i in f.read().splitlines():
+#     if i[0]!=">":
+        
+#         dna.append(i)
+
+f =open("rosalind_cons.txt", "r")
+list = f.read().splitlines()
+
+
+dna=[]
+for line in "".join(list).split(">")[1:]:
+    dna.append(line[13:])
+
+
+A=[]
+C=[]
+G=[]
+T=[]
+consensus=[]
+dic={"A":0, "C":0, "G":0, "T":0}
+for n in range(0, len(dna[0])):
+    for i in range(0, len(dna)):
+        dic[dna[i][n]]+=1
+    A.append(dic.get("A"))
+    C.append(dic.get("C"))
+    G.append(dic.get("G"))
+    T.append(dic.get("T"))
+    top= max(dic, key=dic.get)
+    consensus.append(top)
+    dic={"A":0, "C":0, "G":0, "T":0}
+
+print("".join(consensus))
+print("A:", " ".join(map(str, A)))
+print("C:", " ".join(map(str, C)))
+print("G:", " ".join(map(str, G)))
+print("T:", " ".join(map(str, T)))
+
+
